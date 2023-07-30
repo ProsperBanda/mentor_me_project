78d85e4c83d1b5503d315b83b8671ad55c9e5adc
Prosper Banda <prosperbanda@meta.com>
2023-07-30 16:52:10 -0700

    Created the mentee notifications component

---
+++ mentor_me-ui/src/components/MenteeNotifications.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Notifications = () => {
+  return (
+    <div className="notifications-content">
+      <h3>Notifications</h3>
+      <p>Notification 1: New mentorship request!</p>
+      <p>Notification 2: Message from Mentee</p>
+    </div>
+  );
+};
+
+export default Notifications;
--- mentor_me-ui/src/components/Sidebar.jsx
+++ mentor_me-ui/src/components/Sidebar.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
+import Notifications from "./MenteeNotifications";
 
 const SideBar = () => {
+  const [showNotifications, setShowNotifications] = useState(false);
+
+  const handleNotificationsClick = () => {
+    setShowNotifications(!showNotifications);
+  };
+
   return (
     <div className="sidebar">
       <button className="sidebar-button">Chat</button>
-      <button className="sidebar-button">Notifications</button>
+      <button className="sidebar-button" onClick={handleNotificationsClick}>
+        Notifications
+      </button>
       <button className="sidebar-button">Requests</button>
+      {showNotifications && (
+        <div className="notifications-popup">
+          <button className="back-arrow" onClick={handleNotificationsClick}>
+            ⬅
+          </button>
+          <Notifications />
+        </div>
+      )}
     </div>
   );
 };
